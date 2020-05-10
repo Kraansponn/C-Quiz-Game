154a343a07f8fb5f5a2fd9b4b59f3899cc4cc766
Kornel <33583478+Kraansponn@users.noreply.github.com>
2020-05-10 18:46:54 +0100

    Clue 1,2,3 done

--- main.c
+++ main.c
@@ -24,10 +24,55 @@ int get_question_count(const char *filepath) {
     return linesCount + 1; //returns the amount of lines +1 as it always shows theres 1 lines less
 }
 
-int compare() {}
+int clue_level_2(char *answer) {
+    for (int i = 0; i < strlen(answer); ++i) {
+        printf("-");
+    }
+}
 
+int clue_level_3(char *answer) {
+    int lenOfAnswer = strlen(answer);
+    if (lenOfAnswer > 2) {
+        printf("%c", answer[0]);
+        for (int i = 0; i < strlen(answer) - 2; ++i) {
+            printf("-");
+        }
+        printf("%c", answer[lenOfAnswer - 1]);
+    } else if (lenOfAnswer == 2) {
+        printf("%c", answer[0]);
+        printf("%c", answer[1]);
+    } else {
+        printf("%c", answer[0]);
+    }
+}
+
+int clue_level_4(char *answer) {
+    int lenOfAnswer = strlen(answer);
+    for (int j = 0; j < 12; ++j) {
+
+        printf("%d\n", rand() % 50);
+    }
+
+    char clue[100];
+
+    if (lenOfAnswer > 2) {
+        printf("%c", answer[0]);
+        for (int i = 0; i < strlen(answer) - 2; ++i) {
+            printf("-");
+        }
+        printf("%c", answer[lenOfAnswer - 1]);
+    } else if (lenOfAnswer == 2) {
+        printf("%c", answer[0]);
+        printf("%c", answer[1]);
+    } else {
+        printf("%c", answer[0]);
+    }
+}
+
+int clue_level_5(){}
 int main() {
     int questionsCount = get_question_count("C:\\Users\\Kornel\\Desktop\\C-Assigment2\\SampleQuestions.txt");
+    int difficultyLevel = 4;
 
     char *questions[questionsCount][100]; // makes array with same size as amount of lines in file
     char *answers[questionsCount][100];  // makes array with same size as amount of answers
@@ -69,38 +114,57 @@ int main() {
     char wordInput[100]; //used to store users answer
     char answerInLowercase[100];
     int correct = 0;
+    char *clue[100];
 
     while (questionsCount > questionsAnswered) {
 
-        printf("\n");
-        printf("%s:", *questions[questionsAnswered]);
-        scanf("%s", wordInput); //gets user input
-
-        for (int i = 0; wordInput[i]; i++) { //makes all the letters lowercase
-            wordInput[i] = tolower(wordInput[i]);
-        }
-
         strncpy(answerInLowercase, *answers[questionsAnswered], 100);
-
 
         for (int i = 0; answerInLowercase[i]; i++) { //makes all the letters lowercase
             answerInLowercase[i] = tolower(answerInLowercase[i]);
         }
 
         char *pos;
-        if ((pos=strchr(answerInLowercase, '\n')) != NULL)
+        if ((pos = strchr(answerInLowercase, '\n')) != NULL) // removes \n from answer so it can be compared
             *pos = '\0';
-        if ((pos=strchr(answerInLowercase, '\r')) != NULL)
+        if ((pos = strchr(answerInLowercase, '\r')) != NULL)// removes \r from answer so it can be compared
             *pos = '\0';
 
-        if (strcmp(answerInLowercase, wordInput) == 0) {
-            correct++;
+        //Clue
+        if (difficultyLevel == 1) {
+            printf("%s?\n", *questions[questionsAnswered]);
+            printf("?");
+        } else if (difficultyLevel == 2) {
+            printf("%s?\n", *questions[questionsAnswered]);
+            clue_level_2(answerInLowercase);
+        } else if (difficultyLevel == 3) {
+            printf("%s?\n", *questions[questionsAnswered]);
+            clue_level_3(answerInLowercase);
+        } else if (difficultyLevel == 4) {
+            printf("%s?\n", *questions[questionsAnswered]);
+            clue_level_4(answerInLowercase);
+        } else if (difficultyLevel == 5) {
+            printf("%s?\n", *questions[questionsAnswered]);
+            clue_level_5(answerInLowercase);
         }
 
-        printf("%s", answerInLowercase);
-        printf("%s", wordInput);
-        printf("%d", correct);
+
+        scanf("%s", wordInput); //gets user input
+
+        for (int i = 0; wordInput[i]; i++) { //makes all the letters lowercase
+            wordInput[i] = tolower(wordInput[i]);
+        }
+
+        if (strcmp(answerInLowercase, wordInput) == 0) {
+            printf("Correct");
+            correct++;
+        } else {
+            printf("Incorrect");
+        }
+
         questionsAnswered++;
+        printf("\n");
     }
+    printf("%d questions answered correctly", correct);
     return 0;
 }
